2a47f111abfac6697ab1b4a971e78f1eac39e3cc
Martina Emmert <tina.emmert@web.de>
2024-01-07 20:47:09 +0100

    add fltk framework and gtk try

---
+++ framework_tester_wip/FLTK_default/FLTK_default/FLTK_default/main.cpp
@@ -0,0 +1,43 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Widget.H>
+#include <FL/names.h>
+#include <iostream>
+
+using namespace std;
+int width = GetSystemMetrics(SM_CXSCREEN);
+int height = GetSystemMetrics(SM_CYSCREEN);
+
+
+class MainWindow : public Fl_Window {
+	public:
+		MainWindow() : Fl_Window(0, 0, width, height)
+		{
+			color(FL_BLACK);
+			label("framework");
+			fullscreen();
+		}
+	private:
+		int handle(int event) override
+		{
+			switch (event)
+			{
+				case FL_PUSH:
+					color(FL_WHITE);
+					redraw();
+					return 1;
+				case FL_RELEASE:
+					color(FL_BLACK);
+					redraw();
+					return 1;
+			}
+			return Fl_Window::handle(event);
+		}
+};
+
+int main(int argc, char** argv)
+{
+	MainWindow window;
+	window.show();
+	return Fl::run();
+}
