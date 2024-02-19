8dc1600f792c7f69f2a18246dc39965ed5b311e6
unknown <tina.emmert@web.de>
2024-02-19 11:56:07 +0100

    fix printf newline windup and clean sh

--- start_measurement_series.sh
+++ start_measurement_series.sh
@@ -1,25 +1,17 @@
 #!/bin/sh
 
-ITERATIONS=200
-DATA_DIR="1602_newsetup"
+ITERATIONS=100
+DATA_DIR="1902_newsetup_100_printf"
 
-#READERS=("getpixel" "bitblt" "getdbits")
+#READERS=("windup" "getpixel" "bitblt" "getdbits")
 #READERS=("getpixel" "bitblt" "getdbits" "pyautogui_reader" "ctypes_reader" "windup_python" "windup")
+READERS=("windup" "getpixel" "bitblt" "getdbits")
 
-READERS=("windup")
-
-#FRAMEWORKS=("GTK" "FLTK" "win32" "pyglet" "Java2D" "JavaSwing" "tkinter" "pyqt5" "pyqt6" "wxpython" "pygame" "SDL2_OpenGLES" "SDL2_OpenGLES2" "SDL2_OpenGL" "SDL2_Software" "SDL2_Direct3D" "GLEW" "GLUT")
-FRAMEWORKS=("FLTK" "win32" "pyglet" "tkinter" "pyqt5" "pyqt6" "wxpython" "pygame" "GLUT" "GTK" "Unity" "psychopy")
-
-#FRAMEWORKS=("Unity")
-#FRAMEWORKS=("psychopy")
-#FRAMEWORKS=("win32")
-
-FRAMEWORKS_SDL=("SDL2_OpenGL" "SDL2_OpenGL_vsync" "SDL2_OpenGLES2" "SDL2_OpenGLES2_vsync" "SDL2_OpenGLES" "SDL2_OpenGLES_vsync" "SDL2_Direct3D" "SDL2_Direct3D_vsync" "GLEW_SDL_vsync" "GLEW_SDL")
-#FRAMEWORKS=("SDL2_OpenGLES2_test")
+#FRAMEWORKS=("FLTK" "win32" "pyglet" "tkinter" "pyqt5" "pyqt6" "wxpython" "pygame" "GLUT" "GTK" "Unity" "SDL2_OpenGL" "SDL2_OpenGLES2" "SDL2_OpenGLES" "SDL2_Direct3D" "SDL2_Software" "GLEW_SDL" "Qt5" "Qt6" "Direct3D" "Java2D" "JavaSwing" "Godot" "psychopy")
+FRAMEWORKS=("FLTK" "win32" "pyglet" "tkinter" "pyqt5" "pyqt6" "wxpython" "pygame" "GLUT" "GTK" "Unity" "SDL2_OpenGL" "SDL2_OpenGLES2" "SDL2_OpenGLES" "SDL2_Direct3D" "SDL2_Software" "GLEW_SDL" "Qt5" "Qt6" "Direct3D" "Java2D" "JavaSwing")
 
 #FRAMEWORKS_GLUMPY=("pyglet" "glfw" "qt5" "sdl")
-#FRAMEWORKS=("win32")
+
 
 
 mkdir "./data/$DATA_DIR"
@@ -72,26 +64,26 @@ done
 
 
 # measure SDLs
-for f in "${!FRAMEWORKS_SDL[@]}";
-do
-    for r in "${!READERS[@]}";
-    do
-        if [ "$f" -eq 0 ] && [ "$r" -eq 0 ]
-        then
-            ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" default True "${READERS[$r]}" $DATA_DIR
-            ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" default False "${READERS[$r]}" $DATA_DIR
-            ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" rects True "${READERS[$r]}" $DATA_DIR
-            ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" rects False "${READERS[$r]}" $DATA_DIR
-        else
-            ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" default True "${READERS[$r]}" $DATA_DIR
-            ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" default False "${READERS[$r]}" $DATA_DIR
-            ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" rects True "${READERS[$r]}" $DATA_DIR
-            ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" rects False "${READERS[$r]}" $DATA_DIR
-        fi
-        r=$((r+1));
-    done
-    f=$((f+1));
-done
+# for f in "${!FRAMEWORKS_SDL[@]}";
+# do
+#     for r in "${!READERS[@]}";
+#     do
+#         if [ "$f" -eq 0 ] && [ "$r" -eq 0 ]
+#         then
+#             ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" default True "${READERS[$r]}" $DATA_DIR
+#             ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" default False "${READERS[$r]}" $DATA_DIR
+#             ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" rects True "${READERS[$r]}" $DATA_DIR
+#             ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" rects False "${READERS[$r]}" $DATA_DIR
+#         else
+#             ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" default True "${READERS[$r]}" $DATA_DIR
+#             ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" default False "${READERS[$r]}" $DATA_DIR
+#             ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" rects True "${READERS[$r]}" $DATA_DIR
+#             ./start_measurement_SDL.sh $ITERATIONS "${FRAMEWORKS_SDL[$f]}" rects False "${READERS[$r]}" $DATA_DIR
+#         fi
+#         r=$((r+1));
+#     done
+#     f=$((f+1));
+# done
 
 
 # # for glumpy frameworks
