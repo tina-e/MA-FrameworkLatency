abf0db459c0dfca448c874d649539b1457a6fe38
unknown <tina.emmert@web.de>
2024-04-05 11:49:46 +0200

    add psychopy and godot data + fixes

--- start_measurement_series.sh
+++ start_measurement_series.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 ITERATIONS=250
-DATA_DIR="easteregg_everything_so_far_250"
+DATA_DIR="easteregg_everything_so_far_250_fix"
 
-READERS=("getpixel" "bitblt" "getdbits" "windup_fullscreen" "none")
+READERS=("windup_fullscreen" "getpixel" "bitblt" "getdbits" "none")
 #READERS=("getpixel" "bitblt" "getdbits" "pyautogui_reader" "ctypes_reader" "windup_python" "windup")
 #FRAMEWORKS=("FLTK" "win32" "pyglet" "tkinter" "pyqt5" "pyqt6" "wxpython" "pygame" "GLUT" "GTK" "SDL2_OpenGL" "SDL2_OpenGLES2" "SDL2_OpenGLES" "SDL2_Direct3D" "SDL2_Software" "GLEW_SDL" "Qt5" "Qt6" "DirectX11" "Java2D" "JavaSwing" "Godot" "Unity" "psychopy_pyglet")
 
@@ -21,6 +21,14 @@ mkdir "./data/$DATA_DIR"
 sleep "7s"
 
 
+for r in "${!READERS[@]}";
+do
+    ./start_measurement.sh $ITERATIONS Godot 3D "${READERS[$r]}" fullscreen $DATA_DIR
+    ./start_measurement.sh $ITERATIONS Godot default "${READERS[$r]}" no_fullscreen $DATA_DIR
+    ./start_measurement.sh $ITERATIONS Godot default "${READERS[$r]}" small $DATA_DIR
+    r=$((r+1));
+done
+
 # default and rects
 # for f in "${!FRAMEWORKS_FULLSCREEN_AND_RECTS[@]}";
 # do
@@ -32,6 +40,50 @@ sleep "7s"
 #     done
 #     f=$((f+1));
 # done
+
+
+# ./start_measurement.sh $ITERATIONS pyqt6 default windup_fullscreen small $DATA_DIR
+# ./start_measurement.sh $ITERATIONS Direct3D default getdbits fullscreen $DATA_DIR
+# ./start_measurement.sh $ITERATIONS Direct3D default getpixel fullscreen $DATA_DIR
+# ./start_measurement.sh $ITERATIONS pyglet default getdbits fullscreen $DATA_DIR
+# ./start_measurement.sh $ITERATIONS pyglet default bitblt fullscreen $DATA_DIR
+
+# todo seltsam
+# ./start_measurement.sh $ITERATIONS pyglet 3D getpixel fullscreen $DATA_DIR
+# ./start_measurement.sh $ITERATIONS pyglet 3D bitblt fullscreen $DATA_DIR
+# ./start_measurement.sh $ITERATIONS pyglet 3D getdbits fullscreen $DATA_DIR
+
+# ./start_measurement.sh $ITERATIONS GLUT 3D windup_fullscreen fullscreen $DATA_DIR
+
+# sleep "3s"
+
+# for r in "${!READERS[@]}";
+# do
+#     ./start_measurement.sh $ITERATIONS Godot default "${READERS[$r]}" fullscreen $DATA_DIR
+#     ./start_measurement.sh $ITERATIONS Godot image "${READERS[$r]}" fullscreen $DATA_DIR
+#     ./start_measurement.sh $ITERATIONS Godot rects "${READERS[$r]}" fullscreen $DATA_DIR
+#     r=$((r+1));
+# done
+
+# sleep "3s"
+
+# for r in "${!READERS[@]}";
+# do
+#     ./start_measurement.sh $ITERATIONS GLEW_SDL default "${READERS[$r]}" fullscreen $DATA_DIR
+#     ./start_measurement.sh $ITERATIONS GLEW_SDL rects "${READERS[$r]}" fullscreen $DATA_DIR
+#     ./start_measurement.sh $ITERATIONS GLEW_SDL image "${READERS[$r]}" fullscreen $DATA_DIR
+#     ./start_measurement.sh $ITERATIONS GLEW_SDL 3D "${READERS[$r]}" fullscreen $DATA_DIR
+#     ./start_measurement.sh $ITERATIONS GLEW_SDL default "${READERS[$r]}" no_fullscreen $DATA_DIR
+#     ./start_measurement.sh $ITERATIONS GLEW_SDL default "${READERS[$r]}" small $DATA_DIR
+#     r=$((r+1));
+# done
+
+# sleep "3s"
+
+# ./start_measurement.sh $ITERATIONS Godot 3D none fullscreen $DATA_DIR
+# ./start_measurement.sh $ITERATIONS psychopy_pyglet rects none fullscreen $DATA_DIR
+# ./start_measurement.sh $ITERATIONS psychopy_pyglet image none fullscreen $DATA_DIR
+
 
 # # condition image
 # for f in "${!FRAMEWORKS_IMAGE[@]}";
@@ -60,46 +112,46 @@ sleep "7s"
 # sleep "3s"
 
 # condition 3D
-for f in "${!FRAMEWORKS_3D[@]}";
-do
-    for r in "${!READERS[@]}";
-    do
-        ./start_measurement.sh $ITERATIONS "${FRAMEWORKS_3D[$f]}" 3D "${READERS[$r]}" fullscreen $DATA_DIR
-        r=$((r+1));
-    done
-    f=$((f+1));
-done
+# for f in "${!FRAMEWORKS_3D[@]}";
+# do
+#     for r in "${!READERS[@]}";
+#     do
+#         ./start_measurement.sh $ITERATIONS "${FRAMEWORKS_3D[$f]}" 3D "${READERS[$r]}" fullscreen $DATA_DIR
+#         r=$((r+1));
+#     done
+#     f=$((f+1));
+# done
 
-sleep "3s"
+# sleep "3s"
 
 # small
-for f in "${!FRAMEWORKS_SMALL[@]}";
-do
-    for r in "${!READERS[@]}";
-    do
-        ./start_measurement.sh $ITERATIONS "${FRAMEWORKS_SMALL[$f]}" default "${READERS[$r]}" small $DATA_DIR
-        r=$((r+1));
-    done
-    f=$((f+1));
-done
+# for f in "${!FRAMEWORKS_SMALL[@]}";
+# do
+#     for r in "${!READERS[@]}";
+#     do
+#         ./start_measurement.sh $ITERATIONS "${FRAMEWORKS_SMALL[$f]}" default "${READERS[$r]}" small $DATA_DIR
+#         r=$((r+1));
+#     done
+#     f=$((f+1));
+# done
 
-sleep "3s"
+# sleep "3s"
 
 # for glumpy frameworks
-for f in "${!FRAMEWORKS_GLUMPY[@]}";
-do
-    for r in "${!READERS[@]}";
-    do
-        ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" default "${READERS[$r]}" fullscreen $DATA_DIR
-        ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" rects "${READERS[$r]}" fullscreen $DATA_DIR
-        ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" 3D "${READERS[$r]}" fullscreen $DATA_DIR
-        ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" image "${READERS[$r]}" fullscreen $DATA_DIR
-        ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" default "${READERS[$r]}" no_fullscreen $DATA_DIR
-        ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" default "${READERS[$r]}" small $DATA_DIR
-        r=$((r+1));
-    done
-    f=$((f+1));
-done
+# for f in "${!FRAMEWORKS_GLUMPY[@]}";
+# do
+#     for r in "${!READERS[@]}";
+#     do
+#         ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" default "${READERS[$r]}" fullscreen $DATA_DIR
+#         ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" rects "${READERS[$r]}" fullscreen $DATA_DIR
+#         ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" 3D "${READERS[$r]}" fullscreen $DATA_DIR
+#         ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" image "${READERS[$r]}" fullscreen $DATA_DIR
+#         ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" default "${READERS[$r]}" no_fullscreen $DATA_DIR
+#         ./start_measurement_glumpy.sh $ITERATIONS "${FRAMEWORKS_GLUMPY[$f]}" default "${READERS[$r]}" small $DATA_DIR
+#         r=$((r+1));
+#     done
+#     f=$((f+1));
+# done
 
 # final all
 # for f in "${!FRAMEWORKS[@]}";
