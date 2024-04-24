c29de89b369646a80606414f0e9ef9c4c05b0c42
Martina Emmert <tina.emmert@web.de>
2024-04-24 18:41:32 +0200

    cleanup main

--- framework_tester_wip/JavaFX_default/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafx_default {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafx_default to javafx.fxml;
-    exports com.example.javafx_default;
-}
--- framework_tester_wip/JavaFX_rects/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafx_rects {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafx_rects to javafx.fxml;
-    exports com.example.javafx_rects;
-}
