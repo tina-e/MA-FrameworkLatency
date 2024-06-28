3a7b980fd2ce482278ef3377e575cf36f4bf1fcb
unknown <tina.emmert@web.de>
2024-06-28 17:52:03 +0200

    add final data hopefully

--- start_measurement.sh
+++ start_measurement.sh
@@ -12,69 +12,69 @@ FW_PATH="framework_tester_apps/$TEST_APP/${TEST_APP}_${TEST_COMPLEXITY}_${FULLSC
 
 case "$TEST_APP" in 
 
-    *"psychopy"*)
-        cd ./framework_tester_projects/framework_tester_applied/psychopy/psychopy_venv
-        source ./Scripts/activate
-        python ../psychopy_${TEST_COMPLEXITY}.py &
-        PID_TEST_PROGRAM=$!
-        # deactivate
-        cd ../../../../
-        ;;
+    # *"psychopy"*)
+    #     cd ./framework_tester_projects/framework_tester_applied/psychopy/psychopy_venv
+    #     source ./Scripts/activate
+    #     python ../psychopy_${TEST_COMPLEXITY}.py &
+    #     PID_TEST_PROGRAM=$!
+    #     # deactivate
+    #     cd ../../../../
+    #     ;;
 
-    *"OpenSesame_PsychoPy"*)
-        ;;
+    # *"OpenSesame_PsychoPy"*)
+    #     ;;
 
-    *"Java"*)
-        java ./$FW_PATH.java &
-        PID_TEST_APP=$!
-        ;;
+    # *"Java"*)
+    #     java ./$FW_PATH.java &
+    #     PID_TEST_APP=$!
+    #     ;;
 
-    *"py"*)
-        python ./$FW_PATH.py &
-        PID_TEST_APP=$! 
-        ;;
+    # *"py"*)
+    #     python ./$FW_PATH.py &
+    #     PID_TEST_APP=$! 
+    #     ;;
 
-    *"Skia"*)
-        python ./$FW_PATH.py &
-        PID_TEST_APP=$! 
-        ;;
+    # *"Skia"*)
+    #     python ./$FW_PATH.py &
+    #     PID_TEST_APP=$! 
+    #     ;;
 
-    *"tkinter"*)
-        python ./$FW_PATH.py &
-        PID_TEST_APP=$! 
-        ;;
+    # *"tkinter"*)
+    #     python ./$FW_PATH.py &
+    #     PID_TEST_APP=$! 
+    #     ;;
 
-    *"SDL"*)
-        start "./framework_tester_apps/SDL2/${TEST_APP}_${TEST_COMPLEXITY}_${FULLSCREEN_OPTION}.exe" &
-        PID_TEST_APP=$! 
-        ;;
+    # *"SDL"*)
+    #     start "./framework_tester_apps/SDL2/${TEST_APP}_${TEST_COMPLEXITY}_${FULLSCREEN_OPTION}.exe" &
+    #     PID_TEST_APP=$! 
+    #     ;;
 
-    *"Unity"*)
-        start "./framework_tester_projects/framework_tester_applied/Unity_${TEST_COMPLEXITY}/framework.exe" &
-        PID_TEST_APP=$! 
-        ;;
+    # *"Unity"*)
+    #     start "./framework_tester_projects/framework_tester_applied/Unity_${TEST_COMPLEXITY}/framework.exe" &
+    #     PID_TEST_APP=$! 
+    #     ;;
 
-    *"Godot"*)
-        start "./framework_tester_projects/framework_tester_applied/Godot/Godot_${TEST_COMPLEXITY}.exe" &
-        PID_TEST_APP=$! 
-        ;;
+    # *"Godot"*)
+    #     start "./framework_tester_projects/framework_tester_applied/Godot/Godot_${TEST_COMPLEXITY}.exe" &
+    #     PID_TEST_APP=$! 
+    #     ;;
 
-    *"UnrealEngine5"*)
-        start "./framework_tester_projects/framework_tester_applied/UnrealEngine5/out/out_${TEST_COMPLEXITY}/Windows/invisible_character.exe" &
-        PID_TEST_APP=$! 
-        ;;
+    # *"UnrealEngine5"*)
+    #     start "./framework_tester_projects/framework_tester_applied/UnrealEngine5/out/out_${TEST_COMPLEXITY}/Windows/invisible_character.exe" &
+    #     PID_TEST_APP=$! 
+    #     ;;
 
-    *"Direct3D"*)
-        cd "./framework_tester_apps/Direct3D_${TEST_COMPLEXITY}"
-        start "./Direct3D_${TEST_COMPLEXITY}_${FULLSCREEN_OPTION}.exe" &
-        PID_TEST_APP=$! 
-        cd ../../
-        ;;
+    # *"Direct3D"*)
+    #     cd "./framework_tester_apps/Direct3D_${TEST_COMPLEXITY}"
+    #     start "./Direct3D_${TEST_COMPLEXITY}_${FULLSCREEN_OPTION}.exe" &
+    #     PID_TEST_APP=$! 
+    #     cd ../../
+    #     ;;
 
-    *)
-        start ./$FW_PATH.exe &
-        PID_TEST_APP=$! 
-        ;;
+    # *)
+    #     start ./$FW_PATH.exe &
+    #     PID_TEST_APP=$! 
+    #     ;;
 
 esac
 
@@ -89,14 +89,14 @@ trap sigint INT
 python ./fyalmd.py $ITERATIONS $TEST_APP $TEST_COMPLEXITY $PIXEL_READER $FULLSCREEN_OPTION $DATA_DIR 
 PID_FYALMD=$!
 
-TSKILL $PID_TEST_APP
-# kill -9 $PID_TEST_APP
-powershell kill -n "${TEST_APP}_${TEST_COMPLEXITY}_${FULLSCREEN_OPTION}"
-powershell kill -n "${TEST_APP}_${TEST_COMPLEXITY}"
-powershell kill -n "framework"
-powershell kill -n "UnrealGame"
-powershell kill -n "java"
-powershell kill -n "python"
+# TSKILL $PID_TEST_APP
+# # kill -9 $PID_TEST_APP
+# powershell kill -n "${TEST_APP}_${TEST_COMPLEXITY}_${FULLSCREEN_OPTION}"
+# powershell kill -n "${TEST_APP}_${TEST_COMPLEXITY}"
+# powershell kill -n "framework"
+# powershell kill -n "UnrealGame"
+# powershell kill -n "java"
+# powershell kill -n "python"
 
 # cd ./framework_tester_projects/framework_tester_applied/psychopy/psychopy_venv
 # deactivate
--- start_measurement_series.sh
+++ start_measurement_series.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ITERATIONS=200
-DATA_DIR="last_fixes_d3d_vsync"
+ITERATIONS=50
+DATA_DIR="finals_psycho"
 
 READERS_ALL=("windup" "getpixel" "bitblt" "getdbits" "none")
 READERS=("getpixel" "none" "windup")
@@ -16,9 +16,9 @@ FRAMEWORKS_NOW=("Unity" "Godot")
 # ./start_measurement.sh $ITERATIONS pygame default windup fullscreen $DATA_DIR
 
 mkdir "./data/$DATA_DIR"
-sleep "7s"
+sleep "25s"
 
-./start_measurement.sh $ITERATIONS Direct3D default none fullscreen $DATA_DIR
+./start_measurement.sh $ITERATIONS EPrime image none fullscreen $DATA_DIR
 # ./start_measurement.sh $ITERATIONS Direct3D default getpixel fullscreen $DATA_DIR
 
 
